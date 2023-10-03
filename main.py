fbd0837df1582f1578b5d77e8002b330b19aea63
scekrezi <s.cekrezi@icloud.com>
2023-10-03 14:57:11 -0500

    Input test

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-first_name = input("Please enter your first name: ")
-last_name = input("Please enter your last name: ")
-
-print("Hello", first_name, last_name, "nice to meet you!")
-
-first_name = input("Please enter your first name:")
-last_name = input("Please enter your last name:")
-age = input("How old are you " + first_name + '?')
-print("Hello", first_name, last_name, "nice to meet you!")
-print(first_name, 'you were born in the year', 2023-int(age))
+print("Today is a good day to learn Python")
+print("Python is fun")
+print("Python's string are easy to use")
+print("hello" + " world")
+greeting = "Hello"
+name = input("Please enter your name ")
+print(greeting + name)
+# if we want a space, we can add that too
+print(greeting + '  ' + name)
