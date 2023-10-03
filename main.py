8a8a4bf644c7ef642b447bc773c9c43f187d0414
scekrezi <s.cekrezi@icloud.com>
2023-10-03 14:06:30 -0500

    Input test

---
+++ main.py
@@ -0,0 +1,10 @@
+first_name = input("Please enter your first name: ")
+last_name = input("Please enter your last name: ")
+
+print("Hello", first_name, last_name, "nice to meet you!")
+
+first_name = input("Please enter your first name:")
+last_name = input("Please enter your last name:")
+age = input("How old are you " + first_name + '?')
+print("Hello", first_name, last_name, "nice to meet you!")
+print(first_name, 'you were born in the year', 2023-int(age))
