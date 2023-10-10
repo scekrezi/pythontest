d3f8af8cfb575cc9a4122227e2487602802dc689
scekrezi <s.cekrezi@icloud.com>
2023-10-10 13:27:01 -0500

    Input test

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+# Sindi Cekrezi notes: This function prints the numbers from 1 through 4
+# with a specific word behind that goes like this:
+# Number 1 American Elm,
+# Number 2 Sugar Maple,
+# Number 3 Red Oak,
+# Number 4 American Beach
 print("Number 1\tAmerican Elm")
 print("Number 2\tSugar Maple")
 print("Number 3\tRed Oak")
